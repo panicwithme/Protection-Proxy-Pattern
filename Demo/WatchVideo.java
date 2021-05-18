351e2dbe476fc22cfb877cb64a3e94ba575d99bf
panicwithme <fcigdemtosun@protonmail.com>
2021-05-18 23:39:33 +0300

    Add demo

---
+++ Demo/WatchVideo.java
@@ -0,0 +1,44 @@
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WatchVideo {
+    public static void main(String[] args) {
+        new WatchVideo().run();
+    }
+
+    public void run() {
+        
+        User user = new User("Cigdem", "Basic", buildVideoList());
+
+        try {
+            user.watchVideo("Football Game");
+        } catch (Exception e) {
+            System.out.println("Access denied. You must upgrade.");
+        }
+    }
+
+    public List<Video> buildVideoList() {
+
+        List<Video> videoList = new ArrayList<Video>();
+
+        Video houseMD = new VideoImpl("House MD", "Series");
+        InvocationHandler houseMDHandler = new InvocationHandlerImpl(houseMD);
+        Video houseMDProxy = (Video) Proxy.newProxyInstance(Video.class.getClassLoader(), new Class[]{Video.class}, houseMDHandler);
+
+        Video bladeRunner = new VideoImpl("Blade Runner", "Movie");
+        InvocationHandler bladeRunnerHandler = new InvocationHandlerImpl(bladeRunner);
+        Video bladeRunnerProxy = (Video) Proxy.newProxyInstance(Video.class.getClassLoader(), new Class[]{Video.class}, bladeRunnerHandler);
+
+        Video footballGame = new VideoImpl("Football Game", "Sports");
+        InvocationHandler footballGameHandler = new InvocationHandlerImpl(footballGame);
+        Video footballGameProxy = (Video) Proxy.newProxyInstance(Video.class.getClassLoader(), new Class[]{Video.class}, footballGameHandler);
+       
+        videoList.add(houseMDProxy);
+        videoList.add(bladeRunnerProxy);
+        videoList.add(footballGameProxy);
+
+        return videoList;
+    }
+}
